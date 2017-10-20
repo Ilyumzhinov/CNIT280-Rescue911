9fee4c36530316508f97118076cd44671386c4d8
Muhammad Zeeshan Z Khalid <khalidm@purdue.edu>
2017-10-20 01:17:52 +0000

    Visual Changes to the amin form + Call Waiting Form Imported from Changeset 178

--- Rescue-911/Rescue-911/Form1.Designer.cs
+++ Rescue-911/Rescue-911/Form1.Designer.cs
@@ -30,27 +30,72 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.btnWaitingCall = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(44, 202);
+            this.button1.BackColor = System.Drawing.Color.Teal;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(250, 164);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(300, 50);
             this.button1.TabIndex = 0;
             this.button1.Text = "Log call";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnWaitingCall
             // 
-            this.btnWaitingCall.Location = new System.Drawing.Point(44, 257);
+            this.btnWaitingCall.BackColor = System.Drawing.Color.IndianRed;
+            this.btnWaitingCall.FlatAppearance.BorderColor = System.Drawing.Color.IndianRed;
+            this.btnWaitingCall.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWaitingCall.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWaitingCall.ForeColor = System.Drawing.Color.White;
+            this.btnWaitingCall.Location = new System.Drawing.Point(250, 236);
             this.btnWaitingCall.Name = "btnWaitingCall";
-            this.btnWaitingCall.Size = new System.Drawing.Size(75, 23);
+            this.btnWaitingCall.Size = new System.Drawing.Size(300, 50);
             this.btnWaitingCall.TabIndex = 1;
             this.btnWaitingCall.Text = "Waiting call";
-            this.btnWaitingCall.UseVisualStyleBackColor = true;
+            this.btnWaitingCall.UseVisualStyleBackColor = false;
             this.btnWaitingCall.Click += new System.EventHandler(this.btnWaitingCall_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Heavy", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(617, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Emergency Response Information Team";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(12, 527);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Development Team:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Gray;
+            this.label3.Location = new System.Drawing.Point(12, 540);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(453, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Artem Ilyumzhinov, Muhammad Zeeshan Khalid, Shuai Zhang, Jordan Sexton";
             // 
             // Form1
             // 
@@ -58,11 +103,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnWaitingCall);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Rescue-911";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +119,9 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnWaitingCall;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
