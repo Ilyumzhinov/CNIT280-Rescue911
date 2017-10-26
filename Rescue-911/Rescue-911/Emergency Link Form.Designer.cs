c51774bdbd32bcd3bf32590fd88241e71b55fe5e
Artem Ilyumzhinov <ilyumzh@purdue.edu>
2017-10-26 02:37:31 +0000

    Added a separate Link Emergency Form Imported from Changeset 195

---
+++ Rescue-911/Rescue-911/Emergency Link Form.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Rescue_911
+{
+    partial class Emergency_Link_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstEmergencies = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCreateEmergency1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstEmergencies
+            // 
+            this.lstEmergencies.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader2});
+            this.lstEmergencies.GridLines = true;
+            this.lstEmergencies.Location = new System.Drawing.Point(12, 85);
+            this.lstEmergencies.MultiSelect = false;
+            this.lstEmergencies.Name = "lstEmergencies";
+            this.lstEmergencies.ShowItemToolTips = true;
+            this.lstEmergencies.Size = new System.Drawing.Size(560, 97);
+            this.lstEmergencies.TabIndex = 11;
+            this.lstEmergencies.UseCompatibleStateImageBehavior = false;
+            this.lstEmergencies.View = System.Windows.Forms.View.Details;
+            this.lstEmergencies.Visible = false;
+            this.lstEmergencies.SelectedIndexChanged += new System.EventHandler(this.lstEmergencies_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Emergency ID";
+            this.columnHeader1.Width = 80;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Time Recevied";
+            this.columnHeader4.Width = 130;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "State";
+            this.columnHeader5.Width = 72;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Description";
+            this.columnHeader2.Width = 271;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Heavy", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(434, 26);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "LINK THE EMERGENCY CALL TO EMERGENCY";
+            // 
+            // btnCreateEmergency1
+            // 
+            this.btnCreateEmergency1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnCreateEmergency1.FlatAppearance.BorderSize = 0;
+            this.btnCreateEmergency1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCreateEmergency1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreateEmergency1.ForeColor = System.Drawing.Color.White;
+            this.btnCreateEmergency1.Location = new System.Drawing.Point(418, 253);
+            this.btnCreateEmergency1.Name = "btnCreateEmergency1";
+            this.btnCreateEmergency1.Size = new System.Drawing.Size(218, 35);
+            this.btnCreateEmergency1.TabIndex = 21;
+            this.btnCreateEmergency1.Text = "Link to emergency";
+            this.btnCreateEmergency1.UseVisualStyleBackColor = false;
+            this.btnCreateEmergency1.Click += new System.EventHandler(this.btnCreateEmergency1_Click);
+            // 
+            // Emergency_Link_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(648, 300);
+            this.Controls.Add(this.btnCreateEmergency1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstEmergencies);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Emergency_Link_Form";
+            this.Text = "Emergency Link Form";
+            this.Load += new System.EventHandler(this.Emergency_Link_Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstEmergencies;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCreateEmergency1;
+    }
+}
---
+++ Rescue-911/Rescue-911/Emergency Link Form.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Rescue_911
+{
+    public partial class Emergency_Link_Form : Form
+    {
+        private Shared_Data SD;
+        private Emergency_Call Current_Call;
+        private int emergencySelected;
+
+        public Emergency_Link_Form(ref Shared_Data xSD, Emergency_Call xEC)
+        {
+            SD = xSD;
+            Current_Call = xEC;
+
+            InitializeComponent();
+        }
+
+        private void Emergency_Link_Form_Load(object sender, EventArgs e)
+        {
+            lstEmergencies_Fetch();
+        }
+
+        private void lstEmergencies_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                emergencySelected = lstEmergencies.SelectedIndices[0];
+            }
+            catch
+            {
+
+            }
+        }
+
+
+        public void lstEmergencies_Fetch()
+        {
+            if (SD.Emergencies[0] == null)
+            {
+                lstEmergencies.Visible = false;
+                return;
+            }
+            lstEmergencies.Visible = true;
+
+            foreach (Emergency existingEmergency in SD.Emergencies)
+            {
+                int j = 0;
+
+                foreach (Emergency_Call EC in existingEmergency.GetLinkedCalls())
+                {
+                    if (j == 0)
+                    {
+                        ListViewItem lstItem = new ListViewItem(existingEmergency.GetEmergency_ID().ToString());
+
+                        lstItem.SubItems.Add(EC.GetDateTime().ToString("h:mm:ss MM/dd/yyyy "));
+                        lstItem.SubItems.Add(EC.GetState());
+                        lstItem.SubItems.Add(EC.GetDescription());
+
+                        lstEmergencies.Items.AddRange(new ListViewItem[1] { lstItem });
+                    }
+                    else
+                    {
+                        ListViewItem lstItem = new ListViewItem();
+
+                        lstItem.SubItems.Add(EC.GetDateTime().ToString("h:mm:ss MM/dd/yyyy "));
+                        lstItem.SubItems.Add(EC.GetState());
+                        lstItem.SubItems.Add(EC.GetDescription());
+
+                        lstEmergencies.Items.AddRange(new ListViewItem[1] { lstItem });
+                    }
+                    j++;
+                }
+            }
+        }
+
+        //To-Do: Change the method name.
+        private void btnCreateEmergency1_Click(object sender, EventArgs e)
+        {
+            if (emergencySelected == 0)
+                return;
+
+            Emergency_Call[] ECs = SD.Emergencies[emergencySelected].GetLinkedCalls();
+
+
+            SD.Emergencies[int.Parse(lstEmergencies.SelectedItems[0].Text)].GetLinkedCalls()[0].SetState("Declined");
+            ((Form1)SD.OpenForms[2, 0]).UpdateSD(SD);
+
+            lstEmergencies.Items[emergencySelected].SubItems[2].Text = SD.Emergencies[int.Parse(lstEmergencies.SelectedItems[0].Text)].GetLinkedCalls()[0].GetState();
+
+        }
+    }
+}
