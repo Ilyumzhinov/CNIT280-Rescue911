b506ffdf4891eb751aed87fa34c0fa29b1621d0b
kevin <zhan2448@purdue.edu>
2017-10-26 22:23:41 +0000

    Imported from Changeset 206

--- Rescue-911/Rescue-911/Emergency Form.cs
+++ Rescue-911/Rescue-911/Emergency Form.cs
@@ -8,64 +8,28 @@ namespace Rescue_911
     {
         private Shared_Data SD;
         private Emergency_Call Current_Call;
-        private int enmergency_ID;
-        private int Calls_ID;
         private Emergency E = new Emergency();
         public Emergency_Form(ref Shared_Data xSD, Emergency_Call xEC)
         {
             SD = xSD;
-
             Current_Call = xEC;
-
             InitializeComponent();
 
-
-           
         }
-       
-        
-
-     
-        
-
-        private void lbTitle_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void Emergency_Form_Load(object sender, EventArgs e)
         {
-            for (int a = 0; a < SD.Emergencies.Capacity; a++) {
-                if (null == SD.Emergencies[a]) {
-                    enmergency_ID = a;
-                    break;
-                }
-            }
-            for (int a = 0; a < SD.Calls.Capacity; a++)
-            {
-                if (null == SD.Calls[a])
-                {
-                    //Calls_ID = a;
-                    break;
-                }
-            }
 
-
-            E.SetEmergency_ID(enmergency_ID);
-            E.AddLinked_Call(SD.Calls[Calls_ID - 1]);
-        
-
-            lbes.Text = Current_Call.GetTeams_Required().ToString();
+            E.SetEmergency_ID(SD.Emergencies.Capacity - 1);
+            E.AddLinked_Call(Current_Call);
             lbel.Text = Current_Call.GetAddress().ToString();
             lbemergencyid1.Text = E.GetEmergency_ID().ToString();
             txtemergencyd.Text = Current_Call.GetDescription();
             lbes.Text = Current_Call.GetState();
-            
+
         }
         private void btnCreateEmergency1_Click(object sender, EventArgs e)
         {
-
-            SD.Emergencies[enmergency_ID] = E;
+            SD.Emergencies.Add(E);
             ((Form1)SD.OpenForms[2, 0]).UpdateSD(SD);
             this.Close();
         }
