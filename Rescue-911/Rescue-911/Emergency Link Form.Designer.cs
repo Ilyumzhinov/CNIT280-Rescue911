c017f061dab449577a50191aae572ed736e7ae74
Artem Ilyumzhinov <ilyumzh@purdue.edu>
2017-10-26 20:12:33 +0000

    Updated Emergency Linked Form Imported from Changeset 202

--- Rescue-911/Rescue-911/Emergency Link Form.Designer.cs
+++ Rescue-911/Rescue-911/Emergency Link Form.Designer.cs
@@ -194,6 +194,7 @@
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Emergency_Link_Form";
             this.Text = "Emergency Link Form";
+            this.Load += new System.EventHandler(this.Emergency_Link_Form_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Rescue-911/Rescue-911/Emergency Link Form.cs
+++ Rescue-911/Rescue-911/Emergency Link Form.cs
@@ -1,24 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Rescue_911
 {
     public partial class Emergency_Link_Form : Form
     {
-        public Emergency_Link_Form()
+        private Shared_Data SD;
+        private Emergency_Call Current_Call;
+
+        public Emergency_Link_Form(ref Shared_Data xSD, Emergency_Call xCurrent_Call)
         {
+            SD = xSD;
+            Current_Call = xCurrent_Call;
+
             InitializeComponent();
         }
 
         private void btnLinkEmergency_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void Emergency_Link_Form_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
