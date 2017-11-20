605ecb709346f6b3a1963f79e8fe2e454e2f4ad8
Artem Ilyumzhinov <ilyumzh@purdue.edu>
2017-11-20 22:04:00 +0000

    Yes, and this one was not added also Imported from Changeset 267

---
+++ Rescue-911/Rescue-911/Patient Information View.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Rescue_911
+{
+    partial class Patient_Information_View
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtComplications = new System.Windows.Forms.TextBox();
+            this.lblComplications = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblBirthDate = new System.Windows.Forms.Label();
+            this.lblBloodType = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtBloodType = new System.Windows.Forms.TextBox();
+            this.txtBirthdate = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtComplications
+            // 
+            this.txtComplications.Location = new System.Drawing.Point(17, 226);
+            this.txtComplications.Margin = new System.Windows.Forms.Padding(2);
+            this.txtComplications.Multiline = true;
+            this.txtComplications.Name = "txtComplications";
+            this.txtComplications.ReadOnly = true;
+            this.txtComplications.Size = new System.Drawing.Size(480, 54);
+            this.txtComplications.TabIndex = 22;
+            // 
+            // lblComplications
+            // 
+            this.lblComplications.AutoSize = true;
+            this.lblComplications.Location = new System.Drawing.Point(14, 211);
+            this.lblComplications.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblComplications.Name = "lblComplications";
+            this.lblComplications.Size = new System.Drawing.Size(72, 13);
+            this.lblComplications.TabIndex = 21;
+            this.lblComplications.Text = "Complications";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(336, 163);
+            this.lblAge.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(26, 13);
+            this.lblAge.TabIndex = 20;
+            this.lblAge.Text = "Age";
+            // 
+            // lblBirthDate
+            // 
+            this.lblBirthDate.AutoSize = true;
+            this.lblBirthDate.Location = new System.Drawing.Point(321, 73);
+            this.lblBirthDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblBirthDate.Name = "lblBirthDate";
+            this.lblBirthDate.Size = new System.Drawing.Size(54, 13);
+            this.lblBirthDate.TabIndex = 19;
+            this.lblBirthDate.Text = "Birth Date";
+            // 
+            // lblBloodType
+            // 
+            this.lblBloodType.AutoSize = true;
+            this.lblBloodType.Location = new System.Drawing.Point(14, 163);
+            this.lblBloodType.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblBloodType.Name = "lblBloodType";
+            this.lblBloodType.Size = new System.Drawing.Size(61, 13);
+            this.lblBloodType.TabIndex = 18;
+            this.lblBloodType.Text = "Blood Type";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 73);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Name";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(381, 160);
+            this.txtAge.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.ReadOnly = true;
+            this.txtAge.Size = new System.Drawing.Size(116, 20);
+            this.txtAge.TabIndex = 16;
+            // 
+            // txtBloodType
+            // 
+            this.txtBloodType.Location = new System.Drawing.Point(84, 160);
+            this.txtBloodType.Margin = new System.Windows.Forms.Padding(2);
+            this.txtBloodType.Name = "txtBloodType";
+            this.txtBloodType.ReadOnly = true;
+            this.txtBloodType.Size = new System.Drawing.Size(128, 20);
+            this.txtBloodType.TabIndex = 15;
+            // 
+            // txtBirthdate
+            // 
+            this.txtBirthdate.Location = new System.Drawing.Point(380, 70);
+            this.txtBirthdate.Margin = new System.Windows.Forms.Padding(2);
+            this.txtBirthdate.Name = "txtBirthdate";
+            this.txtBirthdate.ReadOnly = true;
+            this.txtBirthdate.Size = new System.Drawing.Size(116, 20);
+            this.txtBirthdate.TabIndex = 14;
+            this.txtBirthdate.Text = "---------------------------------------------------------------------------------" +
+    "------------------------";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(84, 70);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(128, 20);
+            this.txtName.TabIndex = 13;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(403, 319);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(93, 26);
+            this.btnSearch.TabIndex = 12;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Patient_Information_View
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtComplications);
+            this.Controls.Add(this.lblComplications);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblBirthDate);
+            this.Controls.Add(this.lblBloodType);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtBloodType);
+            this.Controls.Add(this.txtBirthdate);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Patient_Information_View";
+            this.Size = new System.Drawing.Size(510, 355);
+            this.Controls.SetChildIndex(this.btnSearch, 0);
+            this.Controls.SetChildIndex(this.txtName, 0);
+            this.Controls.SetChildIndex(this.txtBirthdate, 0);
+            this.Controls.SetChildIndex(this.txtBloodType, 0);
+            this.Controls.SetChildIndex(this.txtAge, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.lblBloodType, 0);
+            this.Controls.SetChildIndex(this.lblBirthDate, 0);
+            this.Controls.SetChildIndex(this.lblAge, 0);
+            this.Controls.SetChildIndex(this.lblComplications, 0);
+            this.Controls.SetChildIndex(this.txtComplications, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtComplications;
+        private System.Windows.Forms.Label lblComplications;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblBirthDate;
+        private System.Windows.Forms.Label lblBloodType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtBloodType;
+        private System.Windows.Forms.TextBox txtBirthdate;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
---
+++ Rescue-911/Rescue-911/Patient Information View.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rescue_911
+{
+    public partial class Patient_Information_View : Special_View
+    {
+        int mSize = 150;
+        int[] age = new int[150];
+        string[] name = new string[150];
+        string[] date = new string[150];
+        string[] bloodtype = new string[150];
+        string[] complications = new string[150];
+        string patientname = "Jane Smith";
+        string patientblood = "O";
+        int patientage = 22;
+        string birthdate = "November 3rd, 1995";
+        string complic = "Had triple heart bypass surgery";
+        int mIndex = 0;
+
+        // CONSTRUCTORS
+        //To-display the view.
+        public Patient_Information_View(ref Shared_Data xSD) : base(ref xSD, "Patient Information", false, Color.Orchid)
+        {
+            InitializeComponent();
+        }
+
+        //To-instantiate the view.
+        public Patient_Information_View() : base("Patient Information", false, Color.Orchid)
+        { }
+        //
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            age[0] = patientage;
+            date[0] = birthdate;
+            name[0] = patientname;
+            bloodtype[0] = patientblood;
+            complications[0] = complic;
+
+            int ctr;
+            for (ctr = 0; ctr < mSize; ctr++)
+            {
+                if (name[mIndex] == txtName.Text)
+                {
+                    int agetemp;
+                    agetemp = age[mIndex];
+                    txtAge.Text = agetemp.ToString();
+                    txtBloodType.Text = bloodtype[mIndex];
+                    txtBirthdate.Text = date[mIndex];
+                    txtComplications.Text = complications[mIndex];
+                    MessageBox.Show("Patient found!");
+                    return;
+                }
+            }
+            MessageBox.Show("Patient not found!");
+        }
+    }
+}
