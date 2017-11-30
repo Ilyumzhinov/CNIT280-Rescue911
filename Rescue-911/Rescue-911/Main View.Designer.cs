4a20740f76aff8cb5c244a8bb0652f8456333138
Muhammad Zeeshan Z Khalid <khalidm@purdue.edu>
2017-11-30 22:49:30 +0000

    Got rid of the ManagedAccess package Imported from Changeset 283

--- Rescue-911/Rescue-911/Main View.Designer.cs
+++ Rescue-911/Rescue-911/Main View.Designer.cs
@@ -28,11 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lbDevelopers = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.lbUsers = new System.Windows.Forms.Label();
             this.lbTime = new System.Windows.Forms.Label();
+            this.eMPLOYEEBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.eMPLOYEEBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -40,30 +43,32 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(13, 204);
-            this.label1.Margin = new System.Windows.Forms.Padding(5);
+            this.label1.Location = new System.Drawing.Point(26, 392);
+            this.label1.Margin = new System.Windows.Forms.Padding(10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.Size = new System.Drawing.Size(152, 31);
             this.label1.TabIndex = 3;
             this.label1.Text = "Developers:";
             // 
-            // label2
+            // lbDevelopers
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.DimGray;
-            this.label2.Location = new System.Drawing.Point(13, 225);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(117, 112);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Artem Ilyumzhinov,\r\n\r\nZeeshan Khalid,\r\n\r\nShuai Zhang,\r\n\r\nJordan Sexton";
+            this.lbDevelopers.AutoSize = true;
+            this.lbDevelopers.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDevelopers.ForeColor = System.Drawing.Color.DimGray;
+            this.lbDevelopers.Location = new System.Drawing.Point(26, 433);
+            this.lbDevelopers.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lbDevelopers.Name = "lbDevelopers";
+            this.lbDevelopers.Size = new System.Drawing.Size(229, 217);
+            this.lbDevelopers.TabIndex = 4;
+            this.lbDevelopers.Text = "Artem Ilyumzhinov,\r\n\r\nZeeshan Khalid,\r\n\r\nShuai Zhang,\r\n\r\nJordan Sexton";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(8, 104);
+            this.label3.Location = new System.Drawing.Point(16, 200);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.Size = new System.Drawing.Size(190, 25);
             this.label3.TabIndex = 5;
             this.label3.Text = "Current # of users:";
             // 
@@ -71,9 +76,10 @@
             // 
             this.lbUsers.AutoSize = true;
             this.lbUsers.ForeColor = System.Drawing.Color.MediumSeaGreen;
-            this.lbUsers.Location = new System.Drawing.Point(108, 104);
+            this.lbUsers.Location = new System.Drawing.Point(216, 200);
+            this.lbUsers.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lbUsers.Name = "lbUsers";
-            this.lbUsers.Size = new System.Drawing.Size(14, 13);
+            this.lbUsers.Size = new System.Drawing.Size(24, 25);
             this.lbUsers.TabIndex = 6;
             this.lbUsers.Text = "#";
             // 
@@ -81,28 +87,31 @@
             // 
             this.lbTime.AutoSize = true;
             this.lbTime.ForeColor = System.Drawing.Color.Silver;
-            this.lbTime.Location = new System.Drawing.Point(8, 117);
+            this.lbTime.Location = new System.Drawing.Point(16, 225);
+            this.lbTime.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lbTime.Name = "lbTime";
-            this.lbTime.Size = new System.Drawing.Size(45, 13);
+            this.lbTime.Size = new System.Drawing.Size(86, 25);
             this.lbTime.TabIndex = 7;
             this.lbTime.Text = "at TIME";
             // 
             // Main_View
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.lbTime);
             this.Controls.Add(this.lbUsers);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbDevelopers);
             this.Name = "Main_View";
-            this.Size = new System.Drawing.Size(300, 459);
-            this.Controls.SetChildIndex(this.label2, 0);
+            this.Padding = new System.Windows.Forms.Padding(20, 19, 20, 19);
+            this.Size = new System.Drawing.Size(717, 883);
+            this.Controls.SetChildIndex(this.lbDevelopers, 0);
             this.Controls.SetChildIndex(this.label1, 0);
             this.Controls.SetChildIndex(this.label3, 0);
             this.Controls.SetChildIndex(this.lbUsers, 0);
             this.Controls.SetChildIndex(this.lbTime, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.eMPLOYEEBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,9 +120,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbDevelopers;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label lbUsers;
         private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.BindingSource eMPLOYEEBindingSource;
     }
 }
--- Rescue-911/Rescue-911/Main View.cs
+++ Rescue-911/Rescue-911/Main View.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using Oracle.DataAccess.Client;
 
 namespace Rescue_911
 {
@@ -27,7 +29,23 @@ namespace Rescue_911
             lbTime.Text = DateTime.Now.ToString("h:mm:ss MM/dd/yyyy");
             lbUsers.Text = SD.FormsCount.ToString();
 
+            // TEST DATABASE CONNECTION
+           // OleDbConnection cn = new OleDbConnection("DATA SOURCE=oracle.ecn.purdue.edu:1521/master.ecn.purdue.edu;PASSWORD=group1814;USER ID=RESCUE911DB");
+            //cn.Open();
+            OracleConnection cmd1 = new OracleConnection();
+            cmd1.ConnectionString = "Data Source= oracle.ecn.purdue.edu:1521/master.ecn.purdue.edu;" +
+                "User Id=rescue911db;" +
+                "Password = group1814;";
+            cmd1.Open();
+
+            OracleCommand command = cmd1.CreateCommand();
+            command.CommandText = "desc Operator; ";
+            //OracleDataReader odr = command.ExecuteReader();
+
+            //lbDevelopers.Text = (string)odr[0];
+
+            //
+
         }
-        //
     }
 }
