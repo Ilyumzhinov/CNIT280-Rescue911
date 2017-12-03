015c46f9fd13ff75468fadd6e994846742355a11
kevin <zhan2448@purdue.edu>
2017-12-03 01:37:53 +0000

    Imported from Changeset 302

--- Rescue-911/Rescue-911/Emergency Add View.cs
+++ Rescue-911/Rescue-911/Emergency Add View.cs
@@ -10,24 +10,35 @@ namespace Rescue_911
     {
         private Special_List<Emergency_Call> Calls;
         private Emergency_Call Current_Call;
+        private Special_List<Emergency> Emergency;
         private Emergency E = new Emergency();
-       
-        public Emergency_Add_View(bool toDisplay, ref Special_List<Emergency_Call> xCalls, Emergency_Call xEC) : this(toDisplay)
+        private int temp = 0;
+        public Emergency_Add_View(bool toDisplay,ref Special_List<Emergency> xEmer, ref Special_List<Emergency_Call> xCalls, Emergency_Call xEC) : this(toDisplay)
         {
             Calls = xCalls;
             Current_Call = xEC;
+            Emergency = xEmer;
+            
+            for (int a = 0; a < xCalls.Count; a++) {
+                if (xCalls[a].GetEmergency().GetEmergency_ID() > temp) {
 
-           // E.SetEmergency_ID(SD.GetEmergencies()[SD.GetEmergencies().Count - 1].GetEmergency_ID() + 1);
+                    temp = xCalls[a].GetEmergency().GetEmergency_ID();
+                }
+                
 
+            }
+           
+            E.SetEmergency_ID(temp + 1);
+            Current_Call.SetEmergency(E);
             // Change taht
             E.SetEType("broken bones");
-            Current_Call.SetEmergency(E);
+           
             lbel.Text = Current_Call.GetAddress().ToString();
-            lbemergencyid1.Text = E.GetEmergency_ID().ToString();
+            lbemergencyid1.Text = (temp + 1).ToString();
             txtemergencyd.Text = Current_Call.GetDescription();
             lbes.Text = Current_Call.GetState();
         }
-
+        
         public Emergency_Add_View(bool toDisplay) : base(toDisplay, "Add Emergency", Color.GreenYellow)
         {
             if (toDisplay)
@@ -37,7 +48,7 @@ namespace Rescue_911
      
         public void btnCreateEmergency1_Click(object sender, EventArgs e)
         {
-            Current_Call.SetState("Logged");
+           
             lbes.Text = "Logged";
             Calls.Add(Current_Call);
 
