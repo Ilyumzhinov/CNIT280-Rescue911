343e7952dfdd3c40122225adf8f23510294e2d21
Muhammad Zeeshan Z Khalid <khalidm@purdue.edu>
2017-12-04 01:37:56 +0000

    BS class added Imported from Changeset 316

---
+++ Rescue-911/Rescue-911/BaseStation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rescue_911
+{
+   public class BaseStation
+    {
+        private int BS_ID;
+        private string BS_Name;
+
+        public int GetBS_ID() { return BS_ID; }
+
+    }
+}
