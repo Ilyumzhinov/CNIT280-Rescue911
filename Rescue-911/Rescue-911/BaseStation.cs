1bf31a32477839b7458a39edd7b0cc3afd4297c7
Muhammad Zeeshan Z Khalid <khalidm@purdue.edu>
2017-12-04 01:47:25 +0000

    Base Station class done, added more get and set methods Imported from Changeset 318

--- Rescue-911/Rescue-911/BaseStation.cs
+++ Rescue-911/Rescue-911/BaseStation.cs
@@ -6,12 +6,46 @@ using System.Threading.Tasks;
 
 namespace Rescue_911
 {
-   public class BaseStation
+    public class BaseStation
     {
         private int BS_ID;
         private string BS_Name;
+        private string BS_Location;
 
-        public int GetBS_ID() { return BS_ID; }
+        public BaseStation()
+        {
+        }
+
+        public int GetBS_ID()
+        {
+            return BS_ID;
+        }
+
+        public string GetBS_Name()
+        {
+            return BS_Name;
+        }
+
+        public string GetBS_Location()
+        {
+            return BS_Location;
+        }
+
+        public void SetBS_ID(int xBS_ID)
+        {
+            this.BS_ID = xBS_ID;
+        }
+
+        public void SetBS_Name(string xBS_Name)
+        {
+            this.BS_Name = xBS_Name;
+        }
+
+        public void SetBS_Location(string xBS_Location)
+        {
+            this.BS_Location = xBS_Location;
+        }
+
 
     }
 }
